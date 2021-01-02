0175667c2d5eacdd429bc50d95c3c611bdf12ef0
lekaiser <sylvanus010@gmail.com>
2021-01-02 15:19:54 +0100

    Fix compilation bugs after maven including

--- src/main/java/csveditor/Main.java
+++ src/main/java/csveditor/Main.java
@@ -2,17 +2,15 @@ package csveditor;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("editor.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/editor.fxml"));
         primaryStage.setTitle("CSV files extractor & editor");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
